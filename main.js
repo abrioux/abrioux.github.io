3dbc12439d61817a02fa9d7ce6f2ee11ec1f92f3
Alex Brioux <abrioux25@gmail.com>
2015-05-11 21:51:27 -0400

    Added main.js file
    Beginning some work with Jquery

---
+++ main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('div').fadeIn('slow');
+    $('.pagebody').fadeIn('slow');
+    //$("#home").fadeIn(1000);
+	//$('#about_me').fadeIn(1500);
+	//$('#linkedin').fadeIn(2000);
+	//$('#contact_me').fadeIn(2500);
+});

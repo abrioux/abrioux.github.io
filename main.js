4d8c2ee40a9cca62ca4158dc02ba2ab12c8b1edd
Alex Brioux <abrioux25@gmail.com>
2015-05-11 22:28:20 -0400

    Revert "Modified Tracking"

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-    $('p').fadeIn(10000);
-    $("#home").fadeIn(1000);
-	$('#about_me').fadeIn(1500);
-	$('#linkedin').fadeIn(2000);
-	$('#contact_me').fadeIn(2500);
+    $('div').fadeIn(10000);
+    //$("#home").fadeIn(1000);
+	//$('#about_me').fadeIn(1500);
+	//$('#linkedin').fadeIn(2000);
+	//$('#contact_me').fadeIn(2500);
 });

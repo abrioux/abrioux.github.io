1cce67e55d08874dff6857197dedc4fb2b726e0c
Alex Brioux <abrioux25@gmail.com>
2015-07-25 18:36:35 -0400

    Site Over-haul
    Entirely new site design and structure using updated practices

--- main.js
+++ main.js
@@ -1,5 +1,48 @@
 $(document).ready(function() {
-	$('.nav li').each(function(i){
-		$(this).delay((i++) * 500).fadeTo(1000,1);
-	})
+	jQuery.easing.def = "easeInOutSine";
+	$('#logo-header').click(function(event){
+		event.preventDefault();
+        $("html, body").animate({ 
+		scrollTop: "0px"
+		}, 800);
+    });
+	$('#nav-home').click(function(event){
+		event.preventDefault();
+        $("html, body").animate({ 
+		scrollTop: "0px"
+		}, 800);
+    });
+	$('#nav-about').click(function(event){
+		event.preventDefault();
+        $("html, body").animate({ 
+		scrollTop: $('#about').offset().top
+		}, 800);
+    });
+	$('#about-me-btn').click(function(event){
+		event.preventDefault();
+        $("html, body").animate({ 
+		scrollTop: $('#about').offset().top
+		}, 800);
+    });
+	$('#nav-projects').click(function(event){
+		event.preventDefault();
+        $("html, body").animate({ 
+		scrollTop: $('#projects').offset().top
+		}, 800);
+    });
+	$('#arrow-container').click(function(event){
+		event.preventDefault();
+        $("html, body").animate({ 
+		scrollTop: $('#about').offset().top
+		}, 800);
+    });
+	function sendMail() {
+    var link = "mailto:me@example.com"
+             + "?cc=myCCaddress@example.com"
+             + "&subject=" + escape("This is my subject")
+             + "&body=" + escape(document.getElementById('myText').value)
+    ;
+
+    window.location.href = link;
+}
 });
